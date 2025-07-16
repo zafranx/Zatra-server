30dc708bf61fa0a5482b03dc7df89946ad815e87
Zafran Khan <96617852+zafranx@users.noreply.github.com>
2025-07-16 18:08:02 +0530

    product master schema

---
+++ src/models/ProjectMaster.js
@@ -0,0 +1,62 @@
+// Investment Opportunities fields (City ID, Project Type Drop Down, Project Name,
+//  Project Location, Picture Gallery, Video Gallery, Minimum Investment in INR, Assured ROI (%),
+//  Lock in Period (years), Project Start Date, Completion Deadline, Available Sizes (multiple - Unit, Size), Approval Status (Multiple), Contact Name, 
+// Phone Number, Email Address, Project Developer (s), Banking Partner (s),
+//  Distance from City, Distance from Airport, Distance from Railway Station, Amenities (multiple), Comments
+const mongoose = require("mongoose");
+// Project Master
+const _SchemaDesign = new mongoose.Schema(
+  {
+    CityId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "admin_lookups",
+    },
+    ProjectType: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "admin_lookups",
+    },
+    ProjectName: String,
+    ProjectLocation: String,
+    PictureGallery: [String],
+    VideoGallery: [String],
+    MinimumInvestment: String,
+    AssuredROI: String,
+    LockinPeriod: String,
+    ProjectStartDate: Date,
+    CompletionDeadline: Date,
+    AvailableSizes: [
+      {
+        Unit: {
+          type: mongoose.SchemaTypes.ObjectId,
+          ref: "admin_lookups",
+        },
+        Size: String,
+      },
+    ],
+    ApprovalStatus: [
+      {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "admin_lookups",
+      },
+    ],
+    ContactName: String,
+    PhoneNumber: String,
+    EmailAddress: String,
+    ProjectDeveloper: [String],
+    BankingPartner: [String],
+    DistancefromCity: String,
+    DistancefromAirport: String,
+    DistancefromRailwayStation: String,
+    Amenities: [
+      {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "admin_lookups",
+      },
+    ],
+    Comments: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("project_master", _SchemaDesign);
