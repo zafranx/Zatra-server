8f6b57474a198a1754ed5a23f34c339141a77fa8
Zafran Khan <96617852+zafranx@users.noreply.github.com>
2025-07-16 13:48:01 +0530

    city contact api

---
+++ src/models/GovtPolicyMaster.js
@@ -0,0 +1,26 @@
+// Government Policy Master (City ID, Policy ID, Policy Title, Short Description,
+//  Long Description, Eligibility, Government Authority,  Uploaded Policy Document)
+const mongoose = require("mongoose");
+// Government Policy Master
+const _SchemaDesign = new mongoose.Schema(
+  {
+    CityId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "admin_lookups",
+    },
+    PolicyId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "admin_lookups",
+    },
+    PolicyTitle: String,
+    ShortDesc: String,
+    LongDesc: String,
+    Eligibility: String,
+    GovernmentAuthority: String,
+    PolicyDocument: String, //document
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("helpline_master", _SchemaDesign);
