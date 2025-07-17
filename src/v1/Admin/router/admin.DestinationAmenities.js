8819e3b77c95ebd1bcb38288ad2936fc81883fec
Zafran Khan <96617852+zafranx@users.noreply.github.com>
2025-07-17 15:37:22 +0530

    destination amenities api

--- src/v1/Admin/router/admin.DestinationAmenities.js
+++ src/v1/Admin/router/admin.DestinationAmenities.js
@@ -37,7 +37,7 @@ router.post(
         IsActive,
       };
 
-      if (!_id) {
+      if (!_id || _id === "" || _id === null) {
         const newRec = await DestinationAmenitiesMaster.create(saveData);
         await __CreateAuditLog(
           "destination_amenities_master",
